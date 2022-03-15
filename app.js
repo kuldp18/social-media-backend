6ae96e3a1d0e4cd2a1c0c9c93b8e5e0323a485ff
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-03-15 18:50:02 +0530

    update app.js

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ const app = express();
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config({ path: 'config/config.env' });
 }
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+const postRoutes = require('./routes/post');
+
+// using routes
+app.use('/api', post);
 module.exports = app;
