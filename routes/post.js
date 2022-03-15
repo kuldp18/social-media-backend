b597195b1f3591e709716dffe11f9c959c76bd49
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-03-15 23:12:12 +0530

    added delete post route

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { createPost, likeAndUnlikePost } = require('../controllers/post');
+const {
+  createPost,
+  likeAndUnlikePost,
+  deletePost,
+} = require('../controllers/post');
 const { isAuthenticated } = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/post/upload', isAuthenticated, createPost);
 router.post('/post/like/:id', isAuthenticated, likeAndUnlikePost);
+router.delete('/post/delete/:id', isAuthenticated, deletePost);
 module.exports = router;
