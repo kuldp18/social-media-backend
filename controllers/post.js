8a0982e99d0c91d7daa43ec2b7d4d1e39887f94c
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-03-15 22:59:25 +0530

    added like/unlike routes

--- controllers/post.js
+++ controllers/post.js
@@ -26,3 +26,38 @@ exports.createPost = async (req, res) => {
     });
   }
 };
+
+exports.likeAndUnlikePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: 'Post not found',
+      });
+    }
+
+    if (post.likes.includes(req.user._id)) {
+      const index = post.likes.indexOf(req.user._id);
+      post.likes.splice(index, 1);
+      await post.save();
+      return res.status(200).json({
+        success: true,
+        message: 'Post unliked successfully',
+      });
+    } else {
+      post.likes.push(req.user._id);
+      await post.save();
+      return res.status(200).json({
+        success: true,
+        message: 'Post liked successfully',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { createPost } = require('../controllers/post');
+const { createPost, likeAndUnlikePost } = require('../controllers/post');
 const { isAuthenticated } = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/post/upload', isAuthenticated, createPost);
-
+router.post('/post/like/:id', isAuthenticated, likeAndUnlikePost);
 module.exports = router;
