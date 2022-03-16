04de0cf5a51be3134c9470b04d97ffb0af0c30e4
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-03-16 14:34:50 +0530

    added get posts of following feature

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,7 @@ const {
   createPost,
   likeAndUnlikePost,
   deletePost,
+  getFollowingPosts,
 } = require('../controllers/post');
 const { isAuthenticated } = require('../middlewares/auth');
 const router = express.Router();
@@ -10,4 +11,5 @@ const router = express.Router();
 router.post('/post/upload', isAuthenticated, createPost);
 router.post('/post/like/:id', isAuthenticated, likeAndUnlikePost);
 router.delete('/post/delete/:id', isAuthenticated, deletePost);
+router.get('/posts/following', isAuthenticated, getFollowingPosts);
 module.exports = router;
